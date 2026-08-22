29aa358cc580d24f41f5c12b53af93f101dd91de
saghosh <sahanaghosh8@gmail.com>
2026-08-22 11:37:51 +0530

    correcting folder structure

---
+++ src/test/java/com/example/applicationtwo/ApplicationTwoApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.applicationtwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTwoApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
